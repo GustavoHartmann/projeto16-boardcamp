a08bc607c62178831e0754b40faaad7083d57a3b
Gustavo <gustavolerner.hartmann@gmail.com>
2023-02-13 09:39:37 -0300

    database connection

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
+
+const port = process.env.PORT;
+app.listen(port, () =>
+  console.log(`Server running in port: ${port}`)
+);
